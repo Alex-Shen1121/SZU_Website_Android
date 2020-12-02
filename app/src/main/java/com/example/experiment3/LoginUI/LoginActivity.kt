0559a57551ce08cc6bd5d66cbe20d4aaae4562c6
沈晨玙 <939826879@qq.com>
2020-12-02 16:01:47 +0800

    ActivityCollector.kt类

---
+++ app/src/main/java/com/example/experiment3/LoginUI/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.experiment3.LoginUI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.experiment3.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
